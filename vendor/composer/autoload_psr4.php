5c0031f8c81925771a057f91b4e7bfd225e00ad3
Elbrahms05 <39333467+Elbrahms05@users.noreply.github.com>
2024-08-13 16:59:15 +0100

    Update autoload_psr4.php

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Vendor\\OtpSystem\\' => array($baseDir . '/src'),
+    'elbrahms\\OtpSystem\\' => array($baseDir . '/src'),
 );
